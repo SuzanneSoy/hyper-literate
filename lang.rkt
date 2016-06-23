d949cf960614e82419b6142d30db95442a921461
Georges Dupéron <georges.duperon@gmail.com>
2016-06-23 21:42:57 +0200

    Added #%top-interaction (not the right one, though)

--- lang.rkt
+++ lang.rkt
@@ -2,4 +2,7 @@
 ;; Forked from scribble-lib/scribble/lp/lang/lang2.rkt
 (require "private/common.rkt")
 
-(provide (rename-out [module-begin/doc #%module-begin]))
+(provide (rename-out [module-begin/doc #%module-begin])
+         ;; TODO: this is the #%top-interaction from racket/base, not from the
+         ;; user-specified language.
+         #%top-interaction)
