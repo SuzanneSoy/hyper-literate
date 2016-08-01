f6facffe7a6111905a67b28a3f7ce651011d538d
Georges Dupéron <georges.duperon@gmail.com>
2016-08-01 18:58:27 +0200

    Fixes bug with … (again)

--- private/lp.rkt
+++ private/lp.rkt
@@ -55,7 +55,8 @@
                              (syntax-local-introduce #'here)))
                   (define intr (make-syntax-delta-introducer b a))
                   (syntax-local-lift-expression
-                   (intr #`(quote-syntax (a-chunk (... name) (... expr) ...))
+                   (intr #`(quote-syntax (a-chunk ((... ...) name)
+                                                  ((... ...) expr) ...))
                          'flip))
                   #'(begin))
                 (macro-to-expand-unsyntax)))
